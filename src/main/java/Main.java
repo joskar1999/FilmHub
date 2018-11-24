21f1c77e7ba9c81316bba7b410016b471d20888b
joskar1999 <joskar1999@gmail.com>
2018-11-24 17:26:46 +0100

    Connection between View and Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,13 +1,19 @@
 package main.java;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        //TODO load proper FXML file
+        Parent root = FXMLLoader.load(getClass().getResource("../resources/view/MainView.fxml"));
+        primaryStage.setScene(new Scene(root, 1366, 768));
+        primaryStage.setResizable(false);
+        primaryStage.show();
     }
 
     public static void main(String[] args) {
