ad24bc0cca4782530be8765293271f7060f37bb7
joskar1999 <joskar1999@gmail.com>
2018-12-12 20:20:03 +0100

    Displaying List with Products in proper section
    Currently only in MoviesView, style to be improved, data to be added

---
+++ src/main/java/controller/MoviesController.java
@@ -0,0 +1,32 @@
+package main.java.controller;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ListView;
+import main.java.model.Product;
+import main.java.model.Service;
+import main.java.view.ProductListViewCell;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class MoviesController implements Initializable {
+
+    @FXML
+    private ListView<Product> listView;
+
+    private ObservableList<Product> products;
+
+    public MoviesController() {
+        products = FXCollections.observableArrayList();
+        products.addAll(Service.getProducts());
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        listView.setItems(products);
+        listView.setCellFactory(e -> new ProductListViewCell());
+    }
+}
---
+++ src/main/java/view/ProductListViewCell.java
@@ -0,0 +1,70 @@
+package main.java.view;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.ListCell;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.text.Text;
+import main.java.model.Product;
+
+import java.io.IOException;
+
+public class ProductListViewCell extends ListCell<Product> {
+
+    @FXML
+    private Text title;
+
+    @FXML
+    private Text rating;
+
+    @FXML
+    private Text firstActor;
+
+    @FXML
+    private Text secondActor;
+
+    @FXML
+    private Text thirdActor;
+
+    @FXML
+    private ImageView imageView;
+
+    @FXML
+    private AnchorPane anchorPane;
+
+    private FXMLLoader loader;
+
+    @Override
+    protected void updateItem(Product item, boolean empty) {
+        super.updateItem(item, empty);
+
+        if (empty || item == null) {
+
+            setText(null);
+            setGraphic(null);
+
+        } else {
+            if (loader == null) {
+                loader = new FXMLLoader(getClass().getResource("../../resources/view/ListCell.fxml"));
+                loader.setController(this);
+
+                try {
+                    loader.load();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+            }
+
+            title.setText(item.getTitle());
+            rating.setText(String.valueOf(item.getRating()));
+            imageView.setImage(new Image(String.valueOf(getClass().getResource(
+                    "../../resources/images/" + item.getImage()))));
+
+            setText(null);
+            setGraphic(anchorPane);
+        }
+    }
+}
