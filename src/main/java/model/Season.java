254025cfc2ff338c0fbb63ebc4b41facd9deef4d
joskar1999 <joskar1999@gmail.com>
2018-12-27 20:39:00 +0100

    Creating random series

--- src/main/java/model/Season.java
+++ src/main/java/model/Season.java
@@ -1,11 +1,17 @@
 package main.java.model;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 public class Season {
 
-    private ArrayList<Episode> episodes;
+    private ArrayList<Episode> episodes = new ArrayList<>();
     private int seasonDuration;
+    private Random random = new Random();
+
+    public Season() {
+        createSeason();
+    }
 
     public ArrayList<Episode> getEpisodes() {
         return episodes;
@@ -21,5 +27,12 @@ public class Season {
 
     public void setSeasonDuration(int seasonDuration) {
         this.seasonDuration = seasonDuration;
+    }
+
+    private void createSeason() {
+        int numberOfEpisodes = random.nextInt(4) + 4;
+        for (int i = 0; i < numberOfEpisodes; i++) {
+            episodes.add(new Episode());
+        }
     }
 }
