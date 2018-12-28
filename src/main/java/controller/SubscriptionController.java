3a665a3da9f27f6b77f8fbef74ad0842d8187c18
joskar1999 <joskar1999@gmail.com>
2018-12-28 18:41:56 +0100

    Displaying notification after subscription update

--- src/main/java/controller/SubscriptionController.java
+++ src/main/java/controller/SubscriptionController.java
@@ -1,13 +1,17 @@
 package main.java.controller;
 
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.TextField;
 import main.java.model.Service;
+import main.java.model.SubscriptionType;
 
 import java.math.BigDecimal;
 import java.math.RoundingMode;
+import java.net.URL;
+import java.util.ResourceBundle;
 
-public class SubscriptionController extends Controller {
+public class SubscriptionController extends Controller implements Initializable {
 
     @FXML
     private TextField basicPriceInput;
@@ -27,24 +31,31 @@ public class SubscriptionController extends Controller {
         basicPrice = basicPriceInput.getText();
         familyPrice = familyPriceInput.getText();
         premiumPrice = premiumPriceInput.getText();
+        boolean isChange = false;
 
         if (basicPriceInput.getText() != null && !basicPriceInput.getText().trim().isEmpty()
             && validateInput(basicPrice) != null) {
             BigDecimal price = new BigDecimal(Double.valueOf(validateInput(basicPrice)))
                 .setScale(2, RoundingMode.HALF_EVEN);
             Service.setSubscriptionPrice(price, null, null);
+            isChange = true;
         }
         if (familyPriceInput.getText() != null && !familyPriceInput.getText().trim().isEmpty()
             && validateInput(familyPrice) != null) {
             BigDecimal price = new BigDecimal(Double.valueOf(validateInput(familyPrice)))
                 .setScale(2, RoundingMode.HALF_EVEN);
             Service.setSubscriptionPrice(null, price, null);
+            isChange = true;
         }
         if (premiumPriceInput.getText() != null && !premiumPriceInput.getText().trim().isEmpty()
             && validateInput(premiumPrice) != null) {
             BigDecimal price = new BigDecimal(Double.valueOf(validateInput(premiumPrice)))
                 .setScale(2, RoundingMode.HALF_EVEN);
             Service.setSubscriptionPrice(null, null, price);
+            isChange = true;
+        }
+        if (isChange) {
+            showNotification("FilmHub", "Ceny subskrypcji zaktualizowane");
         }
     }
 
@@ -62,5 +73,19 @@ public class SubscriptionController extends Controller {
             return null;
         }
         return input;
+    }
+
+    private void updateUI() {
+        basicPriceInput.setPromptText(String.valueOf(
+            Service.getSubscription().getPriceMap().get(SubscriptionType.BASIC) + " PLN"));
+        familyPriceInput.setPromptText(String.valueOf(
+            Service.getSubscription().getPriceMap().get(SubscriptionType.FAMILY) + " PLN"));
+        premiumPriceInput.setPromptText(String.valueOf(
+            Service.getSubscription().getPriceMap().get(SubscriptionType.PREMIUM) + " PLN"));
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        updateUI();
     }
 }
